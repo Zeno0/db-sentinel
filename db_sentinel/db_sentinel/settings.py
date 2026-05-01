58a72bc202c28114d6eafcb8c73ba41afe8f9b22
Shekhar Kumar <zenobell143@gmail.com>
2026-05-01 10:49:28 +0000

    typo

--- db_sentinel/db_sentinel/settings.py
+++ db_sentinel/db_sentinel/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("D_USER"),
+        "USER": os.getenv("DB_USER"),
         "PASSWORD": "DB_PASSWORD",
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
