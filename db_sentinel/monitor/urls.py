4d3602e7af107692705f58b8896b2d1706646d87
Shekhar Kumar <zenobell143@gmail.com>
2026-05-01 14:32:14 +0000

    add dashboard

--- db_sentinel/monitor/urls.py
+++ db_sentinel/monitor/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import slow_queries
+from .views import slow_queries, dashboard
 # from .views import run_collector
 
 urlpatterns = [
     path('slow-queries/', slow_queries),
+    path('dashboard/', dashboard),
     # path('run-collector/', run_collector),
 ]
--- db_sentinel/monitor/views.py
+++ db_sentinel/monitor/views.py
@@ -14,3 +14,14 @@ def slow_queries(request):
 def run_collector(request):
     collect_query_stats()
     return JsonResponse({"status": "collector ran"})
+
+def dashboard(request):
+    slow_queries = QueryStat.objects.order_by('-mean_exec_time')[:10]
+    frequent_queries = QueryStat.objects.order_by('-calls')[:10]
+
+    context = {
+        'slow_queries': slow_queries,
+        'frequent_queries': frequent_queries
+    }
+
+    return render(request, 'monitor/dashboard.html', context)
