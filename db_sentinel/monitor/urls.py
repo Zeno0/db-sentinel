ace38eab6879668bd1425d1a815d5b3f10347ab2
Shekhar Kumar <zenobell143@gmail.com>
2026-05-01 12:05:05 +0000

    removing collector for safety

--- db_sentinel/monitor/urls.py
+++ db_sentinel/monitor/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import slow_queries
-from .views import run_collector
+# from .views import run_collector
 
 urlpatterns = [
     path('slow-queries/', slow_queries),
-    path('run-collector/', run_collector),
+    # path('run-collector/', run_collector),
 ]
