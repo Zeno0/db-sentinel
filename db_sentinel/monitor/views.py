346f9b7b774ba712e9b25417f785a9c489be5324
Shekhar Kumar <zenobell143@gmail.com>
2026-05-01 11:18:59 +0000

    some output worthy urls added

--- db_sentinel/monitor/views.py
+++ db_sentinel/monitor/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse
 from .models import QueryStat
+from .collector import collect_query_stats
+
 
 def slow_queries(request):
     data = list(QueryStat.objects.filter(mean_exec_time__gt=100)
                 .values())
     return JsonResponse(data, safe=False)
+
+def run_collector(request):
+    collect_query_stats()
+    return JsonResponse({"status": "collector ran"})
