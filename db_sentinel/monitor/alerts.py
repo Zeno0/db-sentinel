56594a2368a2021a96f2de793312c5c5ff25ac0c
Shekhar Kumar <zenobell143@gmail.com>
2026-05-03 04:40:21 +0000

    another stupid mistake

--- db_sentinel/monitor/alerts.py
+++ db_sentinel/monitor/alerts.py
@@ -2,16 +2,19 @@ import requests
 import os
 
 def send_telegram_alert(message):
-    # print("Sending alert:", message)
+    print("Sending alert:", message)
     token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
-
+    print("TOKEN:", os.getenv("TELEGRAM_BOT_TOKEN"))
+    print("CHAT_ID:", os.getenv("TELEGRAM_CHAT_ID"))
+    print("SENDING MESSAGE...")
     url = f"https://api.telegram.org/bot{token}/sendMessage"
 
     payload = {
         "chat_id": chat_id,
         "text": message
     }
+
 
     try:
         requests.post(url, json=payload)
